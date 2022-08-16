dcc6f57775e82bf76ed8e4f409cdafa84e9996cd
Marcelo Sitonio <marcelo@sitonio.com>
2022-08-16 18:42:07 -0400

    from Marcelo

--- practice1/Controllers/NbaplayerController.cs
+++ practice1/Controllers/NbaplayerController.cs
@@ -13,17 +13,23 @@ namespace practice1.Controllers
         public ActionResult Index()
 
         {
+            List<Nbaplayer> list = new List<Nbaplayer>();
             Nbaplayer thisplayer=   new Nbaplayer();
             thisplayer.id = 1;
             thisplayer.fname = "Kevin";
             thisplayer.lname = "Durant";
             thisplayer.team = "Nets";
+            list.Add(thisplayer);
 
 
+            Nbaplayer thisplayer2 = new Nbaplayer();
+            thisplayer2.id = 2;
+            thisplayer2.fname = "LB";
+            thisplayer2.lname = "James";
+            thisplayer2.team = "Nets";
+            list.Add(thisplayer2);
 
-
-
-            return View(thisplayer);
+            return View(list);
         }
     }
 }
